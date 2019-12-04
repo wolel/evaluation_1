732fe5f833b028b21bee562226db41bde8ac448a
wolel <56022602+wolel@users.noreply.github.com>
2019-12-04 16:33:40 +0100

    calcul de budget

---
+++ script.js
@@ -0,0 +1,19 @@
+//TODO: --------------tableau Depenses----------------
+
+var charges = document.getElementById('charges');
+var electricite =  document.getElementById('electicite');
+var eau = document.getElementById('eau');
+var internet = document.getElementById('internet');
+
+var courses = document.getElementById('courses');
+var transport = document.getElementById('transport');
+
+//TODO: --------------tableau recette----------------
+
+var salaire = document.getElementById('salaire');
+var alloc = document.getElementById('alloc');
+
+//TODO: --------------tableau epargne----------------
+
+
+var epargne = document.getElementById('epargne');
